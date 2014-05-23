6d5c8ce41b6e3befb8a0681922c287bb7f1a81a4
Johann Zelger <zelger@me.com>
2014-05-23 15:09:23 +0200

    split off from techdivision/webserver

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ TechDivision_Server
 ===================
 
 A multithreaded server daemon framework
+Have a look at [`TechDivision_WebServer`](<https://github.com/techdivision/TechDivision_WebServer>) for reference implementation of a webserver using the server framework.
---
+++ src/TechDivision/Server/Interfaces/ModuleInterface.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * \TechDivision\Server\Interfaces\ModuleInterface
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category   Webserver
+ * @package    TechDivision_Server
+ * @subpackage Interfaces
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/TechDivision_Server
+ */
+
+namespace TechDivision\Server\Interfaces;
+
+use TechDivision\Server\Exceptions\ModuleException;
+use TechDivision\Http\HttpRequestInterface;
+use TechDivision\Http\HttpResponseInterface;
+
+/**
+ * Interface ModuleInterface
+ *
+ * @category   Webserver
+ * @package    TechDivision_Server
+ * @subpackage Interfaces
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/TechDivision_Server
+ */
+interface ModuleInterface
+{
+
+    /**
+     * Implement's module logic for given hook
+     *
+     * @param \TechDivision\Http\HttpRequestInterface  $request  The request object
+     * @param \TechDivision\Http\HttpResponseInterface $response The response object
+     * @param int                                      $hook     The current hook to process logic for
+     *
+     * @return bool
+     * @throws \TechDivision\Server\Exceptions\ModuleException
+     */
+    public function process(HttpRequestInterface $request, HttpResponseInterface $response, $hook);
+
+    /**
+     * Return's an array of module names which should be executed first
+     *
+     * @return array The array of module names
+     */
+    public function getDependencies();
+
+    /**
+     * Returns the module name
+     *
+     * @return string The module name
+     */
+    public function getModuleName();
+
+    /**
+     * Initiates the module
+     *
+     * @param \TechDivision\Server\Interfaces\ServerContextInterface $serverContext The server's context instance
+     *
+     * @return bool
+     * @throws \TechDivision\Server\Exceptions\ModuleException
+     */
+    public function init(ServerContextInterface $serverContext);
+}
